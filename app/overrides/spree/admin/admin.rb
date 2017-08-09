b189647821f8be2620ae742d3b3afcf25bc9288d
Oleg Mozolev <superchell@users.noreply.github.com>
2017-08-09 14:10:27 +0300

    fixed admin menu override url

--- app/overrides/spree/admin/admin.rb
+++ app/overrides/spree/admin/admin.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
     :name => "admin_content_admin_tab_parser",
     :insert_bottom => "[data-hook='admin_tabs']",
-    :text => "<ul class='nav nav-sidebar'><%= tab :mega_menus,  :url => '/mega_menus', :icon => 'th-list' %></ul>",
+    :text => "<ul class='nav nav-sidebar'><%= tab :mega_menus,  :url => '/admin/mega_menus', :icon => 'th-list' %></ul>",
     :disabled => false)
