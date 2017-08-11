96e91fcbd64989d3e2e80bfedde6eb70c928f686
superchel_ <for.oleg.mozolev@gmail.com>
2017-08-11 14:54:37 +0300

    added menu items list columns

--- app/helpers/spree/admin/base_helper_decorator.rb
+++ app/helpers/spree/admin/base_helper_decorator.rb
@@ -1,49 +1,50 @@
 module Spree
   module Admin
     BaseHelper.module_eval do
-      
+
       def mega_menu_build id
-        mega_menu = MegaMenu.find(id).build_menu        
+        mega_menu = MegaMenu.find(id).build_menu
       end
-  
+
       def get_mega_menu_link mega_menu_item, options = {prefix: '', suffix:'', class:'', count:1}
-        
-        url = link = mega_menu_item.link
-        
-        if mega_menu_item.type_of_link == "taxon"
-          
-          if self.is_number? link
-            url =  Spree::Taxon.where(id: mega_menu_item.link).first
-            
-          elsif link.is_a? String
-            url = Spree::Taxon.find_by_name(mega_menu_item.link)
-            
-          end
-          
-          if url.is_a? Spree::Taxon
-              url = seo_url(url)
-          end
-        end
-        
+
+        url = mega_menu_item_link mega_menu_item
+
         # Build menu link
         "<a class='cd-dropdown-item' href='#{url}'>
           #{options[:prefix]} #{mega_menu_item.title} #{options[:suffix]}
         </a>".html_safe
       end
-      
+
       def is_number? string
         true if Float(string) rescue false
       end
-      
-      def column_name num 
+
+      def column_name num
         case num
-        when 1 
-          "left"
-        when 2
-          "right"
+          when 1
+            "left"
+          when 2
+            "right"
         end
       end
-      
+
+      def mega_menu_item_link mega_menu_item
+        url = link = mega_menu_item.link
+
+        if mega_menu_item.type_of_link == "taxon"
+          if self.is_number? link
+            url =  Spree::Taxon.where(id: mega_menu_item.link).first
+          elsif link.is_a? String
+            url = Spree::Taxon.find_by_name(mega_menu_item.link)
+          end
+          url = seo_url(url) if url.is_a? Spree::Taxon
+        end
+
+        url = 'javascript:void(0);' if url == request.filtered_path
+        url
+      end
+
     end
   end
 end
--- app/models/spree/mega_menu.rb
+++ app/models/spree/mega_menu.rb
@@ -1,13 +1,13 @@
 module Spree
   class MegaMenu < ActiveRecord::Base
-    
+
     has_many :mega_menu_items, dependent: :destroy
-    
+
     def build_menu
-      self.mega_menu_items.where(top_level: true).order("weight ASC").map do |i| 
-        {item: i, children: i.children} 
+      self.mega_menu_items.where(top_level: true).order("weight ASC").map do |i|
+        i.children.map{|c| c[:parent_name] = i.title}
+        {item: i, children: i.children}
       end
     end
-
   end
 end
